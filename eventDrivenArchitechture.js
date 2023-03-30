a8dc18acfd952b50f62961bf9f62d10ff9849025
Surendra-Mundvadiya <97657580+Surendra-Mundvadiya@users.noreply.github.com>
2023-03-30 19:25:31 +0530

    initial commit

---
+++ eventDrivenArchitechture.js
@@ -0,0 +1,23 @@
+// Microservice 1
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("hello", (name) => {
+    console.log(`Hello ${name} from Microservice 1!`);
+});
+
+setTimeout(() => {
+    eventEmitter.emit("hello", "World");
+}, 5000);
+
+// Microservice 2
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("hello", (name) => {
+    console.log(`Hello ${name} from Microservice 2!`);
+});
+
+setTimeout(() => {
+    eventEmitter.emit("hello", "World");
+}, 10000);
